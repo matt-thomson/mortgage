3f783f0584c0987e16bb5966ff8553b50171bb20
Matt Thomson <mattjohnthomson@gmail.com>
2015-08-05 18:46:24 +0100

    Start creating Elm app

---
+++ client/Mortgage.elm
@@ -0,0 +1,10 @@
+module Mortgage where
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+main: Html
+main =
+  div
+    [ class "container" ]
+    [ h1 [] [ text "Mortgage Calculator" ] ]
