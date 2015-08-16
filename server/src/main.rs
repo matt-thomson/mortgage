98a25fc0c50799ce3f282d2baaca80f74b69b3f8
Matt Thomson <mattjohnthomson@gmail.com>
2015-08-16 20:55:49 +0100

    Serve client

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,11 @@
 extern crate bodyparser;
 extern crate iron;
+extern crate mount;
 extern crate num;
 extern crate plugin;
 extern crate router;
 extern crate rustc_serialize;
+extern crate staticfile;
 
 mod core;
 mod error;
@@ -14,13 +16,21 @@ pub use error::MortgageError;
 pub use model::{Mortgage, MortgageStats, MortgageWithStats};
 
 use iron::prelude::*;
+use mount::Mount;
 use router::Router;
+use staticfile::Static;
 
 use std::net::Ipv4Addr;
 
 fn main() {
-    let mut router = Router::new();
-    router.post("/mortgages", server::create);
+    let mut api = Router::new();
+    api.post("/", server::create);
 
-    Iron::new(router).http((Ipv4Addr::new(0, 0, 0, 0), 3000)).unwrap();
+    let client = Static::new("client/");
+
+    let mut mount = Mount::new();
+    mount.mount("/", client);
+    mount.mount("/mortgages", api);
+
+    Iron::new(mount).http((Ipv4Addr::new(0, 0, 0, 0), 3000)).unwrap();
 }
