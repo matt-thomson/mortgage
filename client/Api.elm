3fe0201e5e0d667f826e944ca2ce71b2a3defb88
Matt Thomson <mattjohnthomson@gmail.com>
2016-05-12 20:32:46 +0100

    Upgrade to Elm 0.17

--- client/Api.elm
+++ client/Api.elm
@@ -1,39 +1,44 @@
-module Api where
+module Api exposing (..)
 
 import Http
 import Json.Decode exposing ((:=))
 import Json.Encode
 import Task exposing (Task)
 
-type alias Request
-  = { amount: Int
-    , num_years: Int
-    , apr: Float
+
+type alias Request =
+    { amount : Int
+    , num_years : Int
+    , apr : Float
     }
 
-type alias Response
-  = { stats: Stats }
 
-type alias Stats
-  = { monthly_repayment: Float }
+type alias Response =
+    { stats : Stats }
+
+
+type alias Stats =
+    { monthly_repayment : Float }
+
 
 getStats : Request -> Task String Response
 getStats request =
-  let
-    decodeStats =
-      Json.Decode.object1 Stats ("monthly_repayment" := Json.Decode.float)
-    decodeResponse =
-      Json.Decode.object1 Response ("stats" := decodeStats)
+    let
+        decodeStats =
+            Json.Decode.object1 Stats ("monthly_repayment" := Json.Decode.float)
 
-    encodedRequest =
-      Json.Encode.object
-        [ ("amount", Json.Encode.int request.amount)
-        , ("num_years", Json.Encode.int request.num_years)
-        , ("apr", Json.Encode.float request.apr)
-        ]
+        decodeResponse =
+            Json.Decode.object1 Response ("stats" := decodeStats)
 
-    body =
-      Http.string (Json.Encode.encode 0 encodedRequest)
-  in
-    Http.post decodeResponse "/mortgages" body
-    |> Task.mapError toString
+        encodedRequest =
+            Json.Encode.object
+                [ ( "amount", Json.Encode.int request.amount )
+                , ( "num_years", Json.Encode.int request.num_years )
+                , ( "apr", Json.Encode.float request.apr )
+                ]
+
+        body =
+            Http.string (Json.Encode.encode 0 encodedRequest)
+    in
+        Http.post decodeResponse "/mortgages" body
+            |> Task.mapError toString
--- client/Mortgage.elm
+++ client/Mortgage.elm
@@ -1,164 +1,173 @@
-module Mortgage where
+module Mortgage exposing (..)
 
 import Api
-import Effects exposing (Effects)
 import Html exposing (Html, Attribute)
+import Html.App as Html
 import Html.Attributes
 import Html.Events
-import Signal exposing (Address)
-import StartApp exposing (App)
 import String
 import Task
 
-type alias Model
-  = { amount: String
-    , numYears: String
-    , apr: String
-    , response: Result String Api.Response
+
+type alias Model =
+    { amount : String
+    , numYears : String
+    , apr : String
+    , response : Result String Api.Response
     }
 
-type Action
-  = UpdateAmount String
-  | UpdateNumYears String
-  | UpdateApr String
-  | Submit
-  | HandleResponse (Result String Api.Response)
 
-init: (Model, Effects Action)
+type Msg
+    = UpdateAmount String
+    | UpdateNumYears String
+    | UpdateApr String
+    | Submit
+    | Response Api.Response
+    | RequestError String
+
+
+init : ( Model, Cmd Msg )
 init =
-  let
-    model =
-      { amount = ""
-      , numYears = ""
-      , apr = ""
-      , response = Err ""
-      }
-  in
-    (model, Effects.none)
+    let
+        model =
+            { amount = ""
+            , numYears = ""
+            , apr = ""
+            , response = Err ""
+            }
+    in
+        ( model, Cmd.none )
 
-request: Model -> Result String Api.Request
+
+request : Model -> Result () Api.Request
 request model =
-  (String.toInt model.amount
-    `Result.andThen` \amount -> String.toInt model.numYears
-    `Result.andThen` \numYears -> String.toFloat model.apr
-    |> Result.map (\apr -> { amount = amount, num_years = numYears, apr = apr }))
+    let
+        amount =
+            String.toInt model.amount
 
-update: Action -> Model -> (Model, Effects Action)
+        numYears =
+            String.toInt model.numYears
+
+        apr =
+            String.toFloat model.apr
+    in
+        case ( amount, numYears, apr ) of
+            ( Ok amount, Ok numYears, Ok apr ) ->
+                Ok (Api.Request amount numYears apr)
+
+            _ ->
+                Err ()
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
-  let
-    fetchStats =
-      Task.fromResult (request model) `Task.andThen` Api.getStats
-        |> Task.toResult
-        |> Task.map HandleResponse
-  in
-    case action of
-      UpdateAmount amount ->
-        ({ model | amount = amount }, Effects.none)
-      UpdateNumYears numYears ->
-        ({ model | numYears = numYears }, Effects.none)
-      UpdateApr apr ->
-        ({ model | apr = apr }, Effects.none)
-      Submit ->
-        (model, Effects.task fetchStats)
-      HandleResponse response ->
-        ({ model | response = response }, Effects.none)
+    let
+        fetchStats =
+            case request model of
+                Ok r ->
+                    Task.perform RequestError Response (Api.getStats r)
 
-view: Address Action -> Model -> Html
-view address model =
-  let
-    inputLabel id label =
-      Html.label
-        [ Html.Attributes.for id
-        , Html.Attributes.class "col-sm-4 control-label"
-        ]
-        [ Html.text label ]
+                Err () ->
+                    Cmd.none
+    in
+        case action of
+            UpdateAmount amount ->
+                ( { model | amount = amount }, Cmd.none )
 
-    inputField id action value =
-      let
-        onInput address =
-          let
-            toMessage address value =
-              Signal.message address (action value)
-          in
-            Html.Events.on "input" Html.Events.targetValue (toMessage address)
-      in
-        Html.input
-          [ Html.Attributes.id id
-          , Html.Attributes.class "form-control"
-          , Html.Attributes.type' "number"
-          , Html.Attributes.required True
-          , Html.Attributes.value value
-          , onInput address
-          ]
-          [ ]
+            UpdateNumYears numYears ->
+                ( { model | numYears = numYears }, Cmd.none )
 
-    input id label action value =
-      Html.div
-        [ Html.Attributes.class "form-group" ]
-        [ inputLabel id label,
-          Html.div
-          [ Html.Attributes.class "col-sm-8" ]
-          [ inputField id action value ]
-        ]
+            UpdateApr apr ->
+                ( { model | apr = apr }, Cmd.none )
 
-    formView =
-      Html.form
-        [ Html.Attributes.class "form-horizontal" ]
-        [ input "amount" "Amount" UpdateAmount model.amount
-        , input "num_years" "Number of years" UpdateNumYears model.numYears
-        , input "apr" "APR (%)" UpdateApr model.apr
-        ]
+            Submit ->
+                ( model, fetchStats )
 
-    button =
-      Html.button
-        [ Html.Attributes.type' "submit"
-        , Html.Attributes.class "btn btn-primary"
-        , Html.Events.onClick address Submit
-        ]
-        [ Html.text "Submit" ]
+            Response response ->
+                ( { model | response = Ok response }, Cmd.none )
 
-    submitView =
-      Html.div
-        [ Html.Attributes.class "form-group" ]
-        [ Html.div
-          [ Html.Attributes.class "col-sm-offset-4 col-sm-8" ]
-          [ button ]
-        ]
+            RequestError error ->
+                ( { model | response = Err error }, Cmd.none )
 
-    responseText =
-      case model.response of
-        Ok response ->
-          let
-            round x = (x * 100 |> floor |> toFloat) / 100.0
-            monthlyRepayment = round response.stats.monthly_repayment
-          in
-            (String.append "Monthly repayment: £" (toString monthlyRepayment))
-        Err err ->
-          err
 
-    responseView =
-      Html.div
-        [ Html.Attributes.class "row" ]
-        [ Html.div
-          [ Html.Attributes.class "col-sm-offset-4 col-sm-8" ]
-          [ Html.strong [] [ Html.text responseText ] ]
-        ]
+view : Model -> Html Msg
+view model =
+    let
+        inputLabel id label =
+            Html.label
+                [ Html.Attributes.for id
+                , Html.Attributes.class "col-sm-4 control-label"
+                ]
+                [ Html.text label ]
 
-  in
-    Html.div
-      [ Html.Attributes.class "container" ]
-      [ Html.h1 [] [ Html.text "Mortgage Calculator" ]
-      , formView
-      , submitView
-      , responseView
-      ]
+        inputField id action value =
+            Html.input
+                [ Html.Attributes.id id
+                , Html.Attributes.class "form-control"
+                , Html.Attributes.type' "number"
+                , Html.Attributes.required True
+                , Html.Attributes.value value
+                , Html.Events.onInput action
+                ]
+                []
 
-app : App Model
-app = StartApp.start { init = init, view = view, update = update, inputs = [] }
+        input id label action value =
+            Html.div [ Html.Attributes.class "form-group" ]
+                [ inputLabel id label
+                , Html.div [ Html.Attributes.class "col-sm-8" ]
+                    [ inputField id action value ]
+                ]
 
-main : Signal Html
-main = app.html
+        formView =
+            Html.form [ Html.Attributes.class "form-horizontal" ]
+                [ input "amount" "Amount" UpdateAmount model.amount
+                , input "num_years" "Number of years" UpdateNumYears model.numYears
+                , input "apr" "APR (%)" UpdateApr model.apr
+                ]
 
-port tasks : Signal (Task.Task Effects.Never ())
-port tasks =
-    app.tasks
+        button =
+            Html.button
+                [ Html.Attributes.type' "submit"
+                , Html.Attributes.class "btn btn-primary"
+                , Html.Events.onClick Submit
+                ]
+                [ Html.text "Submit" ]
+
+        submitView =
+            Html.div [ Html.Attributes.class "form-group" ]
+                [ Html.div [ Html.Attributes.class "col-sm-offset-4 col-sm-8" ]
+                    [ button ]
+                ]
+
+        responseText =
+            case model.response of
+                Ok response ->
+                    let
+                        round x =
+                            (x * 100 |> floor |> toFloat) / 100.0
+
+                        monthlyRepayment =
+                            round response.stats.monthly_repayment
+                    in
+                        (String.append "Monthly repayment: £" (toString monthlyRepayment))
+
+                Err err ->
+                    err
+
+        responseView =
+            Html.div [ Html.Attributes.class "row" ]
+                [ Html.div [ Html.Attributes.class "col-sm-offset-4 col-sm-8" ]
+                    [ Html.strong [] [ Html.text responseText ] ]
+                ]
+    in
+        Html.div [ Html.Attributes.class "container" ]
+            [ Html.h1 [] [ Html.text "Mortgage Calculator" ]
+            , formView
+            , submitView
+            , responseView
+            ]
+
+
+main : Program Never
+main =
+    Html.program { init = init, update = update, view = view, subscriptions = \_ -> Sub.none }
